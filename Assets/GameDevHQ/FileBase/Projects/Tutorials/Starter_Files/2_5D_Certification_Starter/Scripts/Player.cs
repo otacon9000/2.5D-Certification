883cc629664d863c737a6c0c10b9db8a7694a002
andrea <andrea.alicino@gmail.com>
2021-06-29 15:07:48 +0200

    player model and move added

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;    
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 10.0f;
+    private Vector3 _direction;
+    private CharacterController _cc;
+
+    private void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+        if(_cc == null)
+        {
+            Debug.LogError("CharacterController is NULL");
+        }
+    }
+
+    private void Update()
+    {
+
+        PlayerMove();
+    
+    }
+
+    void PlayerMove()
+    {
+        if (_cc.isGrounded == true)
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            _direction = new Vector3(0, 0, horizontalInput) * _speed;
+            ;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _direction.y += _jumpHeight;
+            }
+            //do things
+        }
+        else
+        {
+
+        }
+
+        _direction.y -= _gravity * Time.deltaTime;
+        _cc.Move(_direction * Time.deltaTime);
+    }
+}
