1bc65d389c1228d5ca708c1b56d2bbc698a64a78
andrea <andrea.alicino@gmail.com>
2021-07-16 19:33:53 +0200

    Moving Platform

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MovingPlatform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _pointA, _pointB;
+    [SerializeField]
+    private float _speed = 1.0f;
+    private bool _switching = true;
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(_switching == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pointB.position, Time.deltaTime * _speed);
+        }
+        else if (_switching == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pointA.position, Time.deltaTime * _speed);
+        }
+
+        if (transform.position == _pointB.position)
+        {
+            _switching = false;
+        }
+        else if (transform.position == _pointA.position)
+        {
+            _switching = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+
+        }
+    }
+}
